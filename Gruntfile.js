f26d0adf8776a68b5f6017e51d7d6163a29dc5b4
Tian Pu <tpu@atlassian.com>
2015-05-11 21:34:57 +1000

    Add grunt-contrib-watch livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,21 +6,32 @@ module.exports = function (grunt) {
     xpkg: grunt.file.readJSON('package.json'),
 
     connect: {
-      slides: {
+      server: {
         options: {
           hostname: '0.0.0.0',
           port: 8000,
           base: 'src',
-          keepalive: true,
           open: true,
+          livereload: true
         }
       }
-    }
+    },
+
+		watch: {
+      options: {
+        livereload: true
+      },
+      slides: {
+        files: [ 'src/**/*' ]
+      }
+		}
+
   });
   
   // Dependencies
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Serve presentation locally
-  grunt.registerTask('slides', [ 'connect:slides']);
+  grunt.registerTask('slides', [ 'connect', 'watch' ]);
 };
